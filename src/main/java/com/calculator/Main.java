4cf7e437dae3ae7dbb8f6c0c57f882afd3af1e63
iHarshal08 <harshal.p0808@gmail.com>
2025-02-26 11:27:43 +0530

    update Main class

--- src/main/java/com/calculator/Main.java
+++ src/main/java/com/calculator/Main.java
@@ -1,7 +1,9 @@
 package com.calculator;
+import com.calculator.ScientificCalculator;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // System.out.println("Hello, World!");
+        ScientificCalculator.main(new String[]{});
     }
 }
